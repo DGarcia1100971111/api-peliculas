347d96a97c998b7ed991e8d21e81edb6bb6411e2
DGarcia1100971111 <107230342+DGarcia1100971111@users.noreply.github.com>
2022-06-25 17:33:37 -0500

    primer commit

---
+++ routers/favoritos.js
@@ -0,0 +1,41 @@
+import {Router} from "express"
+
+import { favoritosDelete, favoritosPost,favoritosGetBuscarid, favoritosGetBuscar } from "../controllers/favoritos.js";
+import { check } from "express-validator";
+import { validarCampos } from "../middlewares/validar-campos.js";
+import { validarJWT } from "../middlewares/validar-jwt.js";
+import HelpersPeliculas from "../helpers/peliculas.js";
+const router = Router();
+
+// eliminar de favoritos
+router.delete("/",[
+    validarJWT,
+    check('id').isMongoId(),
+    validarCampos
+],favoritosDelete);
+
+
+// listar favoritos por id
+router.get("/listarid/:id",[
+    validarJWT,
+    check('id').isMongoId(),
+  
+    validarCampos
+],favoritosGetBuscarid)
+//jholman tu papá
+router.get("/buscarTitulo",[
+    validarJWT,
+    check('id').isMongoId(),
+    check('id').custom(HelpersPeliculas.existePeliculaById),
+    validarCampos
+],favoritosGetBuscar)
+
+// agregar a favoritos
+router.post("/",[
+    validarJWT,
+    check('usuario',"el usuario es obligatorio").not().isEmpty(),
+    check('pelicula',"la pelicula debe ser obligatoria").not().isEmpty(),
+    validarCampos,
+],favoritosPost)
+
+export default router;
